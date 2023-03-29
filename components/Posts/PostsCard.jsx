93f42cddaef678bfc95e9ee779927f16a3572eca
Aditya Prakash <adityaprakash6688@gmail.com>
2023-03-29 23:43:09 +0530

    post card

--- components/Posts/PostsCard.jsx
+++ components/Posts/PostsCard.jsx
@@ -4,9 +4,23 @@ import styles from "./PostsCard.module.scss";
 function PostsCard() {
   return (
     <div className={styles.postsCardWrapper}>
-      <div className={styles.authorDetailsRow}></div>
-      <div className={styles.postContentWrapper}></div>
-      <div className={styles.postsSocials}></div>
+      <div className={styles.postHeader}>
+        <div className={styles.namecard}>
+          <div className={styles.profileImg}></div>
+          <div className={styles.details}>
+            <p className={styles.authorName}>Alison Burgers</p>
+            <p className={styles.username}>{'@'}alladin897</p>
+          </div>
+        </div>
+      </div>
+      
+      <div className={styles.postContent}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, cupiditate fugiat, facere laborum dicta culpa inventore laudantium sint libero autem delectus impedit mollitia accusamus tempora id necessitatibus at quos accusantium esse incidunt asperiores voluptates? Optio vel autem unde alias facere?
+      </div>
+      
+      <div className={styles.postFooter}>
+        
+      </div>
     </div>
   );
 }
