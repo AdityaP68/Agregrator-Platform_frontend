93aa9264d20e50317f4974acbc69db57cbe6caf2
Aditya Prakash <adityaprakash6688@gmail.com>
2023-04-03 22:18:40 +0530

    create post structure done

--- components/CreatePost/CreatePost.jsx
+++ components/CreatePost/CreatePost.jsx
@@ -1,23 +1,52 @@
 import React from "react";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import PlaceIcon from "@mui/icons-material/Place";
+import ImageIcon from "@mui/icons-material/Image";
+import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
 import styles from "./CreatePost.module.scss";
 
 function CreatePost({ modalState }) {
   return (
     <div className={styles.container}>
-      <div className={styles.userImgWrapper}>
-        <div className={styles.userImg} />
-      </div>
-      <div className={styles.textEditorContainer}>
-        <div
-          className={styles.inputWrapper}
-          onClick={() => {
-            console.log("clicked");
-            modalState((v) => !v);
-          }}
-        >
-          <input type={"text"} placeholder="Start posting in this group" />
+      <div className={styles.textareaRow}>
+        <div className={styles.userImgWrapper}>
+          <div className={styles.userImg} />
         </div>
-        <div className={styles.editorOptions}></div>
+        <div className={styles.textEditorContainer}>
+          <div
+            className={styles.inputWrapper}
+            onClick={() => {
+              console.log("clicked");
+              modalState((v) => !v);
+            }}
+          >
+            <input type={"text"} placeholder="Start posting in this group" />
+          </div>
+          <div className={styles.editorOptions}></div>
+        </div>
+      </div>
+      <div className={styles.attachmentOptionsRow}>
+        <div className={styles.optionsWrapper}>
+          <div className={styles.options}>
+            <CameraAltIcon />
+          </div>
+          <div className={styles.options}>
+            <ImageIcon />
+          </div>
+          <div className={styles.options}>
+            <AttachFileIcon />
+          </div>
+          <div className={styles.options}>
+            <PlaceIcon />
+          </div>
+          <div className={styles.options}>
+            <SentimentVerySatisfiedIcon />
+          </div>
+        </div>
+        <div className={styles.postBtnWrapper}>
+          <button>Post</button>
+        </div>
       </div>
     </div>
   );
