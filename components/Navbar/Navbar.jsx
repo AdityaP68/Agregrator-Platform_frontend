c7ca4b0a9844ec661cf995775718120ec81c7856
Aditya Prakash <adityaprakash6688@gmail.com>
2023-04-03 22:42:49 +0530

    navbar search

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
 import styles from './Navbar.module.scss'
 
 function Navbar() {
   return (
     <nav className={styles.navContainer}>
+      <div className={styles.searchWrapper}>
+        <div className={styles.searchBox}>
+          <SearchIcon className={styles.searchIcon}/>
+          <input type="text" placeholder='Search'/>
+        </div>
+          <div className={styles.btnWrapper}>
+            <button>search</button>
+          </div>
+      </div>
     </nav>
   )
 }
